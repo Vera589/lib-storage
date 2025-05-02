2376a0f51c726d0323dba129899b78a67d79cee5
Vera <vhappyand@gmail.com>
2025-05-02 21:36:39 +0300

    Implement review service and repo.

---
+++ app/repository/review.py
@@ -0,0 +1,41 @@
+from typing import List, Dict
+
+from psycopg import sql
+from psycopg.rows import DictRow
+
+from app.repository.db import BaseRepository
+
+
+class ReviewRepository(BaseRepository):
+    def __init__(self):
+        super().__init__('catalog', 'book_review')
+
+    def find_by_user_id_and_book_id(self, user_id: str, book_id: str) -> DictRow:
+        query = sql.SQL(
+            "SELECT * FROM catalog.book_review WHERE book_id = %(book_id)s AND user_id = %(user_id)s").format()
+        result = self.execute_query(query, {"book_id": book_id, "user_id": user_id})
+        return result[0] if result else None
+
+    def find_by_user_id(self, user_id: str) -> List[DictRow]:
+        query = sql.SQL("SELECT * FROM catalog.book_review WHERE user_id = %(user_id)s").format()
+        return self.execute_query(query, {"user_id": user_id})
+
+    def update_by_user_id_and_book_id(self, user_id: str, book_id: str, data: Dict) -> None:
+        set_clause = sql.SQL(', ').join(
+            sql.SQL("{} = {}").format(
+                sql.Identifier(key),
+                sql.Placeholder(key)
+            ) for key in data.keys()
+        )
+
+        query = sql.SQL("UPDATE {} SET {} WHERE user_id = %(user_id)s book_id = %(book_id)s").format(
+            sql.Identifier(self.schema_name, self.table_name),
+            set_clause
+        )
+        params = {"user_id": user_id, "book_id": book_id, **data}
+        self.execute_command(query, params)
+
+    def delete_by_user_id_and_book_id(self, user_id: str, book_id: str) -> None:
+        query = sql.SQL(
+            "DELETE FROM catalog.book_review WHERE book_id = %(book_id)s AND user_id = %(user_id)s").format()
+        self.execute_command(query, {"book_id": book_id, "user_id": user_id})
---
+++ app/service/review.py
@@ -0,0 +1,85 @@
+from dataclasses import dataclass
+from typing import Any, Union
+
+from fastapi import HTTPException
+from pydantic import BaseModel
+
+from app.repository.book import BookRepository
+from app.repository.review import ReviewRepository
+
+@dataclass
+class Review(BaseModel):
+    book_id: Union[str, None] = None
+    rating: Union[int, None] = None
+    review: Union[str, None] = None
+
+
+class ReviewService:
+
+    def __init__(self):
+        self.review_repository = ReviewRepository()
+        self.book_repository = BookRepository()
+
+    def add_review(self, user_id: str, review: Review) -> None:
+        if self.review_repository.find_by_user_id_and_book_id(user_id, review.book_id) is not None:
+            raise HTTPException(
+                status_code=409,
+                detail=f"Review for book {review.book_id} already exists"
+            )
+        if review.book_id is None:
+            raise HTTPException(status_code=400, detail="Review should have a book_id")
+        if review.rating is None:
+            raise HTTPException(status_code=400, detail="Review should have a rating")
+        if review.rating < 0 or review.rating > 100:
+            raise HTTPException(
+                status_code=400,
+                detail="Review rating should be in range [0, 100]"
+            )
+        if len(review.review) > 500:
+            raise HTTPException(status_code=400, detail="Review text should not exceed 500 symbols")
+        book = self.book_repository.find_by_id(review.book_id)
+        if book is None:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Book with id '{review.book_id}' not found"
+            )
+        self.review_repository.create({
+            "book_id": review.book_id,
+            "user_id": user_id,
+            "rating": review.rating,
+            "review": review.review
+        })
+
+    def get_review(self, user_id: str, book_id: str) -> dict[str, Any]:
+        book = self.book_repository.find_by_id(book_id)
+        if book is None:
+            raise HTTPException(status_code=400, detail=f"Book with id '{book_id}' not found")
+        review = self.review_repository.find_by_user_id_and_book_id(user_id, book_id)
+        if review is None:
+            raise HTTPException(status_code=404, detail=f"Review of the book '{book_id}' not found")
+        return review
+
+    def get_all_reviews(self, user_id: str) -> list[dict[str, Any]]:
+        return self.review_repository.find_by_user_id(user_id)
+
+    def update_review(self, user_id: str, book_id: str, updated_review: Review) -> None:
+        self.get_review(user_id, book_id)
+        updated_fields = {}
+        if updated_review.rating is not None:
+            if updated_review.rating < 0 or updated_review.rating > 100:
+                raise HTTPException(
+                    status_code=400,
+                    detail="Review rating should be in range [0, 100]"
+                )
+            updated_fields["rating"] = updated_review.rating
+        if updated_review.review is not None:
+            if len(updated_review.review) > 500:
+                raise HTTPException(
+                    status_code=400,
+                    detail="Review text should not exceed 500 symbols"
+                )
+            updated_fields["review"] = updated_review.review
+        self.review_repository.update_by_user_id_and_book_id(user_id, book_id, updated_fields)
+
+    def delete_review(self, user_id: str, book_id: str) -> None:
+        self.review_repository.delete_by_user_id_and_book_id(user_id, book_id)
